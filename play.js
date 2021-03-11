b24bae42916609bb5a56461cd06f951ac9ad6fa8
ban-mazin <ban.qaddoori89@gmail.com>
2021-03-11 03:00:01 +0000

    add stdin function

--- play.js
+++ play.js
@@ -1,3 +1,16 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+process.stdin.on('data', (key) => {
+  process.stdout.write('.');
+});
+
+console.log('after callback');
