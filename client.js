45b084b56fe5778f2592f9fbfcc2ff85fa16c772
ban-mazin <ban.qaddoori89@gmail.com>
2021-03-11 04:24:41 +0000

    game

--- client.js
+++ client.js
@@ -1,31 +1,36 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
-  conn.on("connect", function(){
+  conn.on('connect', function(){
     console.log('successfully connected to game server');
   })
 
-  conn.on("data", (data) => {
+  conn.on('data', (data) => {
     console.log(data);
   })
 
-  conn.on("connect", () => {
-    conn.write('Name: BAN');
+  conn.on('connect', () => {
+    console.log('connected');
   })
 
   conn.on('connect', () => {
+    conn.write('Name: BAN');
+  })
+
+ conn.on('connect', () => {
      conn.write('Move: up')
-     setInterval(() => {
-       conn.write('Move: left')
-     }, 200)
-    })
+    setInterval(() => {
+     conn.write('Move: left')
+    }, 200)
+   })
 
   return conn;
 }
--- input.js
+++ input.js
@@ -1,12 +1,33 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
+  stdin.on('data', handleUserInput)
   return stdin;
 }
-process.stdin.on('data', (key) => {
-  process.stdout.write('.');
-});
+const handleUserInput = function(data) {
+  if (data === '\u0003') {
+    process.exit();
+  } else if (data === 'w') {
+    connection.write('Move: up');
+  } else if ( data === 'a') {
+    connection.write('Move: left')
+  } else if ( data === 's') {
+    connection.write('Move: down')
+  } else if ( data === 'd') {
+    connection.write('Move: right')
+  } else if ( data === 'm') {
+    connection.write('Say: hello!!! am Done finally')
+  }
+}
 
-console.log('after callback');
+
+module.exports = {
+  setupInput
+}
+// setupInput()
